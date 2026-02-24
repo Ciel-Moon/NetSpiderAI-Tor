8b82d886241a0c53d82a2719982ca1f33ae264e0
Ciel-Moon <chenyichen927527@outlook.com>
2026-02-24 18:23:21 +0800

    Create user_agent.py

---
+++ utils/user_agent.py
@@ -0,0 +1,15 @@
+import random
+from fake_useragent import UserAgent
+
+try:
+    ua = UserAgent()
+    def get_random_ua():
+        return ua.random
+except:
+    _DEFAULT_UAS = [
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Safari/605.1.15",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36"
+    ]
+    def get_random_ua():
+        return random.choice(_DEFAULT_UAS)
